0a836301dc9e8e53e22bdf9e0875d18c3d592a7b
matatych <docmat63@gmail.com>
2024-08-28 18:45:08 +0300

    database edited

--- utils/create_table.sql
+++ utils/create_table.sql
@@ -1,18 +1,21 @@
 CREATE TABLE IF NOT EXISTS masters (
     telegram_name TEXT PRIMARY KEY,
     master_name TEXT,
-    master_nickname TEXT
 );
 
 CREATE TABLE IF NOT EXISTS games (
     game_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    type TEXT CHECK(type IN ('rpg', 'board')),  -- Enum-like constraint
-    genre TEXT,
-    setting TEXT,
+    players_count INTEGER,
     system TEXT,
-    location TEXT,
-    cost INTEGER,
+    setting TEXT,
+    type TEXT CHECK(type IN ('Компания', 'Ваншот', 'Модуль')),
     time TEXT,
+    cost INTEGER,
+    expirience TEXT,
+    free_text,
     master_id TEXT,
     FOREIGN KEY (master_id) REFERENCES masters(telegram_name)
 );
+
+
+
