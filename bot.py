fbe8a17ed9c6c4edc1399d3caf6728f1bb395054
matatych <docmat63@gmail.com>
2024-11-06 21:42:09 +0200

    add Nested Conversations

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
 import logging
-from master import master_conv_handler, start_master_conversation
-from player import player_conv_handler, start_player_conversation
-from telegram import Update, InlineKeyboardButton, ReplyKeyboardMarkup
-from telegram.ext import Application, CommandHandler, ContextTypes, CallbackContext, CallbackQueryHandler, \
-    MessageHandler, filters
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import Application, CommandHandler, ConversationHandler, MessageHandler, filters, CallbackContext
+from master import master_conversation_handler
+from player import player_application_conversation_handler, player_search_conversation_handler
 
 # Включаем логирование
 logging.basicConfig(
@@ -11,16 +10,14 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-
-# Функция обработки команды /start
-
+SELECTION, PLAYER_ACTIONS = range(2)
 
 async def start(update: Update, context: CallbackContext) -> None:
     print('Start clicked')
     reply_keyboard = [
         [
-            InlineKeyboardButton("Мастер", callback_data='master'),
-            InlineKeyboardButton("Игрок", callback_data='player')
+            "Мастер",
+            "Игрок"
         ]
     ]
     await update.message.reply_text(
@@ -29,17 +26,54 @@ async def start(update: Update, context: CallbackContext) -> None:
             reply_keyboard, one_time_keyboard=True, resize_keyboard=True
         ),
     )
+    return SELECTION
 
 
-def main() -> None:
-    application = Application.builder().token(
-        "7530680667:AAFFJ6SxFOcji0z0Aug4xbNaPtzznJ-QSG8").build()
+async def choose_player_actions(update: Update, context: CallbackContext) -> None:
+    print("choose_action() called")
+    print(update.message.text)
 
-    application.add_handler(CommandHandler('start', start))
-    application.add_handler(master_conv_handler)
-    application.add_handler(player_conv_handler)
-    application.run_polling()
+    reply_keyboard = [
+        [
+            "Поиск", "Заявка"
+        ]
+    ]
+    await update.message.reply_text(
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True, resize_keyboard=True
+        ),
+        text="Что хочешь сделать?"
+    )
+    return PLAYER_ACTIONS
+
+
+async def cancel(update: Update, context: CallbackContext) -> int:
+    """End the conversation."""
+    await update.message.reply_text('Пока! Надеюсь, скоро снова пообщаемся.')
+    return ConversationHandler.END
 
 
 if __name__ == '__main__':
-    main()
+    application = Application.builder().token('7530680667:AAFFJ6SxFOcji0z0Aug4xbNaPtzznJ-QSG8').build()
+
+    selection_handlers = [
+        master_conversation_handler,
+        MessageHandler(filters.Regex('^Игрок'), choose_player_actions)
+    ]
+
+    player_selections = [
+        player_search_conversation_handler,
+        player_application_conversation_handler
+    ]
+
+    # Main conversation
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            SELECTION: selection_handlers,
+            PLAYER_ACTIONS: player_selections
+        },
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+    application.add_handler(conv_handler)
+    application.run_polling()
